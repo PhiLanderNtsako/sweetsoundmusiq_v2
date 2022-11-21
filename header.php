f2ae24b5d2503701da5bfb11d63ffb2529c77817
PhiLander <phil.ntsako98@gmail.com>
2022-11-21 19:12:30 +0200

    header update logo path

--- header.php
+++ header.php
@@ -2,11 +2,11 @@
         <nav>
             <ul class="navbar">
             <div class="logo">
-                <img src="http://localhost/musiq-local/img/core-img/logo.png" alt="">
+                <img src="https://www.sweetsound.co.za/musiq/img/core-img/logo.png" alt="">
             </div>
-                <li class="nav-link"><a href="http://localhost/musiq-local/home.php"> Home</a></li>
-                <li class="nav-link"><a href="http://localhost/musiq-local/artists.php"> Artists</a></li>
-                <li class="nav-link"><a href="http://localhost/musiq-local/releases.php"> Releases</a></li>
+                <li class="nav-link"><a href="https://www.sweetsound.co.za/musiq/home.php"> Home</a></li>
+                <li class="nav-link"><a href="https://www.sweetsound.co.za/musiq/artists.php"> Artists</a></li>
+                <li class="nav-link"><a href="https://www.sweetsound.co.za/musiq/releases.php"> Releases</a></li>
             </ul>
         </nav>
     </header>
